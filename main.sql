5c8a0dffbbad74131d7f3cac53b233898ff69208
Inconceivable42 <100614690+Inconceivable42@users.noreply.github.com>
2022-03-09 21:03:31 +0000

    Adds an update statement

--- main.sql
+++ main.sql
@@ -8,11 +8,16 @@ CREATE TABLE IF NOT EXISTS doggy(
   dogid INTEGER NOT NULL PRIMARY KEY,
   dogname VARCHAR(20) NOT NULL,
   breed VARCHAR(20),
-  color VARCHAR(50)
+  color VARCHAR(50),
+  tail VARCHAR(100) 
 );
 INSERT INTO doggy (dogname, breed, color) VALUES
 ("Azor", "Poodle", "brown"),
 ("Pluto", "German Sheppard", "yellow"),
 ("Bobik", "Russian Borzoi", "purple");
 
- SELECT * FROM doggy; 
+UPDATE doggy 
+SET tail="fluffy"
+WHERE dogid=1;
+  
+SELECT * FROM doggy; 
